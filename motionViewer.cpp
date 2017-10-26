40f1f874dd2cec42bf0ae223cc9603c4ac11a10c
Shelley Tian <shelley@Shelleys-MacBook-Air.local>
2017-10-25 18:54:37 -0600

    starting q3

--- motionViewer.cpp
+++ motionViewer.cpp
@@ -31,6 +31,7 @@
 using namespace std;
 
 //Globals
+Joint *root = new Joint();
 vector<Joint*> joints;
 int numFrames;
 float frameTime;
@@ -38,6 +39,16 @@ vector<vector<float>> frames;
 
 
 //Function Prototypes
+void keyInput(unsigned char key, int x, int y);
+void reset();
+
+
+
+
+
+
+
+
 
 
 
@@ -46,7 +57,7 @@ vector<vector<float>> frames;
 
 
 ///////////////////////////////////////////////////////////////////////////////
-/////////////////////////////M   A    I   N////////////////////////////////////
+///////////////////////////  M   A    I   N  //////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]){
   //check file format
@@ -71,7 +82,7 @@ int main(int argc, char *argv[]){
 
   //read hierarchy
   cout << "Reading...\n";
-  Joint *root = new Joint();
+  // Joint *root = new Joint();
   read_hierarchy(infile, root);
   list_tree(root, joints);
 
@@ -82,33 +93,138 @@ int main(int argc, char *argv[]){
   const char *framesLine = line.c_str();
   char w1[10];
   sscanf(framesLine, "%s %d", w1, &numFrames);
-  cout << "numFrames = " << numFrames << endl;
   getline(infile, line); // Frame Time: 0.012312
   const char *frameTimeLine = line.c_str();
   char w2[10];
   sscanf(frameTimeLine, "%s %s %f", w1, w2, &frameTime);
-  cout << "frameTime = " << frameTime << endl;
   read_motion(infile, frames);
   //print_motion(frames);
   infile.close();
-  cout << "done reading.\n-------------------------------\n\n";
+  cout << "Done.\n-------------------------------\n\n";
+
+  ////////////////////////////////////////////////////////
+  glutInit(&argc, argv);
+  glutInitContextVersion(4, 3);
+  glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(100, 100);
+  glutCreateWindow("Animation");
+
+  setup(); //called once
+  glutDisplayFunc(drawScene); //called every time something changes
+  glutReshapeFunc(resize);
+  glutKeyboardFunc(keyInput);
+  glewExperimental = GL_TRUE;
+  glewInit();
+  glutMainLoop();
+
+  return 0;
+
+}
 
 
 
 
 
 
-  //write
-  cout << "Writing...\n";
-  ofstream outfile;
-  outfile.open("output.bvh");
-  //write hierarchy
-  outfile << "HIERARCHY\n";
-  write_hierarchy(outfile, root);
-  outfile << "}\n"; //matches root bracket
 
-  //write motion
-  write_motion(outfile, frames, numFrames, frameTime);
-  outfile.close();
-  cout << "done writing.\n-------------------------------\n\n";
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////  F  U  N  C  T  I  O  N  S  ////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+void drawScene(){
+  glColor3f(1.0, 1.0, 1.0);
+  glPushMatrix();
+  //glCallList(anObject);???
+  glPopMatrix();
+
+  glutSwapBuffers();
+}
+
+
+
+
+void keyInput(unsigned char key, int x, int y){
+   switch(key)
+   {
+      case 'q':
+        exit(0);
+        break;
+      case 'x': //reset
+        reset();
+        break;
+      case 'w':
+        cout << "Writing...\n";
+        ofstream outfile;
+        outfile.open("output.bvh");
+        //write hierarchy
+        outfile << "HIERARCHY\n";
+        write_hierarchy(outfile, root);
+        outfile << "}\n"; //matches root bracket
+
+        //write motion
+        write_motion(outfile, frames, numFrames, frameTime);
+        outfile.close();
+        cout << "Done.\n-------------------------------\n\n";
+        break;
+      case 'p': //pause
+        break;
+
+      //camera control
+      case 'd': //dolly
+      break;
+      case 'D':
+      break;
+      case 'c': //crane
+      break;
+      case 'C':
+      break;
+      case 'z': //zoom
+      break;
+      case 'Z':
+      break;
+      case 't': //tilt
+      break;
+      case 'T':
+      break;
+      case 'a': //pan
+      break;
+      case 'A':
+      break;
+      case 'l': //roll
+      break;
+      case 'L':
+      break;
+
+      //speed control
+      case 43: // +
+        break;
+      case 45: // -
+        break;
+
+      default:
+        break;
+  }
+  glutPostRedisplay();
+}
+
+
+void reset(){
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glutPostRedisplay();
+}
+
+
+void setup(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
 }
